386ac27282fcf57906c18bf1f3a2e513f43987df
stefian <stefan.ianta@w3ai.net>
2020-06-15 14:55:56 -0300

    linked currentUserRouter exported in current-user.ts to the index.ts file of auth service

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,12 +1,12 @@
 import express from "express";
 import { json } from "body-parser";
 
+import { currentUserRouter } from "./routes/current-user";
+
 const app = express();
 app.use(json());
 
-app.get("/api/users/currentuser", (req, res) => {
-  res.send("Hi there! / local dev with Docker Desktop K8s cluster");
-});
+app.use(currentUserRouter);
 
 app.listen(3000, () => {
   console.log("Auth: Listening on port 3000!");
