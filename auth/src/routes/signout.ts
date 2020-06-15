d6e97184e9ba59871abe16339679a3b9c481de1b
stefian <stefan.ianta@w3ai.net>
2020-06-15 16:52:52 -0300

    imported signin, signout and signup routes in auth index.ts

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -2,8 +2,8 @@ import express from "express";
 
 const router = express.Router();
 
-router.get("/api/users/currentuser", (req, res) => {
+router.post("/api/users/signout", (req, res) => {
   res.send("Hi there!");
 });
 
-export { router as currentUserRouter };
+export { router as signoutRouter };
