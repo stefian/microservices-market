27ca1c7f4f54f6bb07ef5f0a46ef4aeb7f33d61e
stefian <stefan.ianta@w3ai.net>
2020-06-16 18:38:17 -0300

    imported and used RequestValidationError & DatabaseConnectionError in signup.ts

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
+import { RequestValidationError } from "../errors/request-validation-error";
+import { DatabaseConnectionError } from "../errors/database-connection-error";
 
 const router = express.Router();
 
@@ -16,13 +18,13 @@ router.post(
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-      throw new Error("Invalid email or password"); // throw Error to be picked up by the errorHandler middleware
+      throw new RequestValidationError(errors.array()); // throw Error to be picked up by the errorHandler middleware
     }
 
     const { email, password } = req.body;
 
     console.log("Creating a user...");
-    throw new Error("Error connecting to database");
+    throw new DatabaseConnectionError();
 
     res.send({});
   }
