d1a88c49d7e75d85a36e71ae6df46f5953228377
stefian <stefan.ianta@w3ai.net>
2020-06-20 15:38:08 -0300

    + jwt.sign() & req.session

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
+import jwt from "jsonwebtoken";
+
 import { User } from "../models/user";
 import { RequestValidationError } from "../errors/request-validation-error";
 import { BadRequestError } from "../errors/bad-request-error";
@@ -32,6 +34,21 @@ router.post(
 
     const user = User.build({ email, password });
     await user.save();
+
+    // Generate JWT
+    const userJwt = jwt.sign(
+      {
+        id: user.id,
+        email: user.email,
+      },
+      "asdf"
+    );
+
+    // Store it on session object
+    // @ts-ignore
+    req.session = {
+      jwt: userJwt,
+    };
 
     res.status(201).send(user);
   }
