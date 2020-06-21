e91f93849b089c1d823e9d8a205b6776ea323be3
stefian <stefan.ianta@w3ai.net>
2020-06-21 15:11:35 -0300

    + toJSON: / userSchema / mongoose

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -21,16 +21,28 @@ interface UserDoc extends mongoose.Document {
   password: string;
 }
 
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
   },
-  password: {
-    type: String,
-    required: true,
-  },
-});
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.password; // JS delete keyword to remove a property from an object
+        delete ret.__v; // or versionKey = false;
+      },
+    },
+  }
+);
 
 userSchema.pre("save", async function (done) {
   if (this.isModified("password")) {
