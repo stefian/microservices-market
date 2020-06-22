04229e48abe526d8083cda2a2bacfa702649ba67
stefian <stefan.ianta@w3ai.net>
2020-06-22 18:24:01 -0300

    + process.env.JWT_JEY / test/setup.ts

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -4,6 +4,8 @@ import { app } from "../app";
 
 let mongo: any;
 beforeAll(async () => {
+  process.env.JWT_KEY = "asdfasdf";
+
   mongo = new MongoMemoryServer();
   const mongoUri = await mongo.getUri();
 
