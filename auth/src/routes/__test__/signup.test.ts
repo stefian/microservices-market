9eab993aadab519c4b671b2193b711eaf4fe102b
stefian <stefan.ianta@w3ai.net>
2020-06-22 21:45:46 -0300

    u - returns a 400 with missing email and password

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -32,5 +32,17 @@ it("returns a 400 with an invalid password", async () => {
 });
 
 it("returns a 400 with missing email and password", async () => {
-  return request(app).post("/api/users/signup").send({}).expect(400);
+  await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.com",
+    })
+    .expect(400);
+
+  await request(app)
+    .post("/api/users/signup")
+    .send({
+      password: "afRGsfg",
+    })
+    .expect(400);
 });
