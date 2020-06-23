3df294aeccfc6fdc1b2d5648a1d017bd97a1d8d6
stefian <stefan.ianta@w3ai.net>
2020-06-22 21:27:47 -0300

    + returns a 400 with an invalid email / signup.test.ts

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -10,3 +10,13 @@ it("returns a 201 on successful signup", async () => {
     })
     .expect(201);
 });
+
+it("returns a 400 with an invalid email", async () => {
+  return request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "testtest.com",
+      password: "password",
+    })
+    .expect(400);
+});
