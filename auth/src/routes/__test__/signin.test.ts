40607b2d2f86b878ccf5ae7d7a4094a5ba261993
stefian <stefan.ianta@w3ai.net>
2020-06-23 09:18:10 -0300

    fails when an incorrect password is supplied

--- auth/src/routes/__test__/signin.test.ts
+++ auth/src/routes/__test__/signin.test.ts
@@ -10,3 +10,21 @@ it("fails when a email that does not exist is supplied", async () => {
     })
     .expect(400);
 });
+
+it("fails when an incorrect password is supplied", async () => {
+  await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.com",
+      password: "password",
+    })
+    .expect(201); // success creating user
+
+  await request(app)
+    .post("/api/users/signin")
+    .send({
+      email: "test@test.com",
+      password: "areeagreg",
+    })
+    .expect(400);
+});
