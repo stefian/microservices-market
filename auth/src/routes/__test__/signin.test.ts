ffd024447e58e7c421be01579578283a825e931e
stefian <stefan.ianta@w3ai.net>
2020-06-23 09:07:33 -0300

    + fails when a email that does not exist is supplied / signin.test.ts

---
+++ auth/src/routes/__test__/signin.test.ts
@@ -0,0 +1,12 @@
+import request from "supertest";
+import { app } from "../../app";
+
+it("fails when a email that does not exist is supplied", async () => {
+  await request(app)
+    .post("/api/users/signin")
+    .send({
+      email: "test@test.com",
+      password: "password",
+    })
+    .expect(400);
+});
