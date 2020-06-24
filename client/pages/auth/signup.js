748b97cccf49cf2e0726b9c5607ee41267b71b05
stefian <stefan.ianta@w3ai.net>
2020-06-24 14:13:18 -0300

    + formatting errors.map() / signup

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -42,7 +42,12 @@ export default () => {
           className="form-control"
         />
       </div>
-      {errors.map(err => err.message)}
+      <div className="alert alert-danger">
+        <h4>Ooops...</h4>
+        <ul className="my-0">
+          {errors.map(err => <li key={err.message}>{err.message}</li>)}
+        </ul>
+      </div>
       <button className="btn btn-primary" >Sign Up</button>
     </form>
   );
