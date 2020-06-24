981246da359d004a3542a6010fcafada8f026e21
stefian <stefan.ianta@w3ai.net>
2020-06-24 12:42:07 -0300

    + email, pass useState / pages/signup.js

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,14 +1,36 @@
+import { useState } from 'react';
+
 export default () => {
-  return <form>
-    <h1>Sign Up</h1>
-    <div className="form-group">
-      <label>Email Address</label>
-      <input className="form-control" />
-    </div>
-    <div className="form-group">
-      <label>Password</label>
-      <input type="password" className="form-control" />
-    </div>
-    <button className="btn btn-primary" >Sign Up</button>
-  </form>;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const onSubmit = (event) => {
+    event.preventDefault(); // to avoid form submitting itself to the browser
+
+    console.log(email, password);
+  };
+
+  return (
+    <form onSubmit={onSubmit} >
+      <h1>Sign Up</h1>
+      <div className="form-group">
+        <label>Email Address</label>
+        <input
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+          className="form-control"
+        />
+      </div>
+      <div className="form-group">
+        <label>Password</label>
+        <input
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+          type="password"
+          className="form-control"
+        />
+      </div>
+      <button className="btn btn-primary" >Sign Up</button>
+    </form>
+  );
 };
