04f381e7df43c49d3cd9910f3c4afed28d8d0ee3
stefian <stefan.ianta@w3ai.net>
2020-06-25 11:38:15 -0300

    + headers: req.headers # to include all headers including cookie

--- client/pages/index.js
+++ client/pages/index.js
@@ -8,16 +8,14 @@ const LandingPage = ({ currentUser }) => { // request executed from the browser
   return <h1>Landing Page</h1>
 }
 
-LandingPage.getInitialProps = async () => {  // Request executed on the server / client k8 pod
+LandingPage.getInitialProps = async ({ req }) => {  // Request executed on the server / client k8 pod
   if (typeof window === 'undefined') {  // window only exists on the browser / not on server
     // we are on the server
     // => requests should be made to http://ingress-nginx-controller.ingress-nginx.svc.cluster.local
     // http://SERVICENAME.NAMESPACE.svc.cluster.local
     const { data } = await axios.get(
       'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser', {
-      headers: {
-        Host: 'aibazar.dev'
-      }
+      headers: req.headers
     }
     );
 
