7ab4fd00bb1cb65238b3aeae630bae8356ca7dc7
stefian <stefan.ianta@w3ai.net>
2020-06-25 19:37:05 -0300

    + appContext.Component.getInitialProps(appContext.ctx) # Passing props trough

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,6 +1,27 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import buildClient from '../api/build-client';
 // Wrapper for all Next Page Components / _app
 // used for global imports in Next - like bootstrap above
-export default ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
+const AppComponent = ({ Component, pageProps, currentUser }) => {
+  return <div>
+    <h1>Header! {currentUser.email}</h1>
+    <Component {...pageProps} />
+  </div>
 };
+
+AppComponent.getInitialProps = async appContext => {
+  const client = buildClient(appContext.ctx);
+  const { data } = await client.get('/api/users/currentuser');
+
+  let pageProps = {};
+  if (appContext.Component.getInitialProps) {
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+  }
+
+  return {
+    pageProps,
+    ...data
+  }
+};
+
+export default AppComponent;
