dfb7f67afeb7324662e4afd1d5e861d1a26a1da6
stefian <stefan.ianta@w3ai.net>
2020-06-25 13:59:53 -0300

    u <h1>You are NOT signed in</h1>

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,7 +1,7 @@
 import buildClient from '../api/build-client';
 
 const LandingPage = ({ currentUser }) => { // request executed from the browser
-  return currentUser ? <h1>You are signed in</h1> : <h1>you are NOT signed in</h1>
+  return currentUser ? <h1>You are signed in</h1> : <h1>You are NOT signed in</h1>
 }
 
 LandingPage.getInitialProps = async (context) => {  // Request executed on the server / client k8 pod
