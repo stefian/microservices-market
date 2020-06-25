38742e5e9cddc29ecb6d047e0db8f0e4d09616e5
stefian <stefan.ianta@w3ai.net>
2020-06-25 09:27:05 -0300

    testing execution of getInitialProps on Server or on Browser # Testing hard Reload, Type URL, Navigation signup => landing /

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,15 +1,18 @@
 import axios from 'axios';
 
-const LandingPage = ({ currentUser }) => {
-  console.log(currentUser);
+const LandingPage = ({ currentUser }) => { // request executed from the browser
+  // console.log(currentUser);
+  // axios.get('/api/users/currentuser'); // from browser baseURL = ''
 
   return <h1>Landing Page</h1>
 }
 
-LandingPage.getInitialProps = async () => {
-  const response = await axios.get('/api/users/currentuser');
+LandingPage.getInitialProps = async () => {  // Request executed on the server / client k8 pod
+  // const response = await axios.get('/api/users/currentuser');
 
-  return response.data;
+  // return response.data;
+  console.log('I WAS EXECUTED');
+  return {};
 };
 
 export default LandingPage;
