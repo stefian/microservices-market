898f2fb64a3e1ad5e8299fd6318e1cf89e78bc33
stefian <stefan.ianta@w3ai.net>
2020-06-25 13:57:08 -0300

    + return currentUser ? <h1>You are signed in</h1> : <h1>you are NOT signed in</h1>

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,16 +1,11 @@
 import buildClient from '../api/build-client';
 
 const LandingPage = ({ currentUser }) => { // request executed from the browser
-  // console.log(currentUser);
-  // axios.get('/api/users/currentuser'); // from browser baseURL = ''
-  console.log(currentUser);
-
-  return <h1>Landing Page</h1>
+  return currentUser ? <h1>You are signed in</h1> : <h1>you are NOT signed in</h1>
 }
 
 LandingPage.getInitialProps = async (context) => {  // Request executed on the server / client k8 pod
   const client = buildClient(context); // build proper axios for server or browser
-
   const { data } = await client.get('/api/users/currentuser');
 
   return data;
