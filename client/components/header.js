32e6cae19248bdaea02a68e4e0bc7b4257eebcbd
stefian <stefan.ianta@w3ai.net>
2020-06-26 07:58:46 -0300

    + links[] / header # signup, signin, signout links

--- client/components/header.js
+++ client/components/header.js
@@ -1,14 +1,28 @@
 import Link from 'next/link';
 
 export default ({ currentUser }) => {
+  const links = [
+    !currentUser && { label: 'Sign Up', href: '/auth/signup' },
+    !currentUser && { label: 'Sign In', href: '/auth/signin' },
+    currentUser && { label: 'Sign Out', href: '/auth/signout' }
+  ]
+    .filter(linkConfig => linkConfig)
+    .map(({ label, href }) => {
+      return <li key={href} className="nav-item">
+        <Link href={href}>
+          <a className="nav-link">{label}</a>
+        </Link>
+      </li>
+    })
+
   return <nav className="navbar nav-light bg-light">
     <Link href="/">
-      <a className="navbar-brand">bazar</a>
+      <a className="navbar-brand">AI Bazar</a>
     </Link>
 
     <div className="d-flex justify-content-end">
       <ul className="nav d-flex align-items-center">
-        {currentUser ? 'Sign out' : 'Sign in/up'}
+        {links}
       </ul>
     </div>
   </nav>
