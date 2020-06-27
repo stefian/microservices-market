093c558f4ebc8372f75d952c1462e8462611ecd8
stefian <stefan.ianta@w3ai.net>
2020-06-27 08:22:45 -0300

    updates to /tikects files

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -4,11 +4,6 @@ import { json } from "body-parser";
 import cookieSession from "cookie-session";
 import { errorHandler, NotFoundError } from "@w3ai/common";
 
-import { currentUserRouter } from "./routes/current-user";
-import { signinRouter } from "./routes/signin";
-import { signoutRouter } from "./routes/signout";
-import { signupRouter } from "./routes/signup";
-
 const app = express();
 app.set("trust proxy", true); // to allow traffic through ingress-nginx
 app.use(json());
@@ -18,11 +13,6 @@ app.use(
     secure: process.env.NODE_ENV !== "test", // to ensure it will always work on https connection or http for testing
   })
 );
-
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
 
 app.all("*", async (req, res) => {
   throw new NotFoundError();
