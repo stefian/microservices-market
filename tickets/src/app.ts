b7eed39964cc1e7516ad18ec6533c9f353252d21
stefian <stefan.ianta@w3ai.net>
2020-06-27 08:13:47 -0300

    cp files from /auth to /tickets

---
+++ tickets/src/app.ts
@@ -0,0 +1,33 @@
+import express from "express";
+import "express-async-errors";
+import { json } from "body-parser";
+import cookieSession from "cookie-session";
+import { errorHandler, NotFoundError } from "@w3ai/common";
+
+import { currentUserRouter } from "./routes/current-user";
+import { signinRouter } from "./routes/signin";
+import { signoutRouter } from "./routes/signout";
+import { signupRouter } from "./routes/signup";
+
+const app = express();
+app.set("trust proxy", true); // to allow traffic through ingress-nginx
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== "test", // to ensure it will always work on https connection or http for testing
+  })
+);
+
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+
+app.all("*", async (req, res) => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app }; // named export
