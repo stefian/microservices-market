536b4acc15aba432542f70dbb6a7243e2ba2419a
stefian <stefan.ianta@w3ai.net>
2020-06-28 17:29:54 -0300

    + it(returns

---
+++ tickets/src/routes/__test__/show.test.ts
@@ -0,0 +1,6 @@
+import request from "supertest";
+import { app } from "../../app";
+
+it("returns a 404 if the ticket is not found", async () => {});
+
+it("returns the ticket if the ticket is found", async () => {});
