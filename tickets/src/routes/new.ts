272da8f026f27df0c5aec189063c6e19a142af84
stefian <stefan.ianta@w3ai.net>
2020-06-28 08:33:54 -0300

    + [body("title").not().isEmpty().withMessage("Title is required")],

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,11 +1,14 @@
 import express, { Request, Response } from "express";
-import { requireAuth } from "@w3ai/common";
+import { body } from "express-validator";
+import { requireAuth, validateRequest } from "@w3ai/common";
 
 const router = express.Router();
 
 router.post(
   "/api/tickets",
   requireAuth,
+  [body("title").not().isEmpty().withMessage("Title is required")],
+  validateRequest,
   (req: Request, res: Response) => {
     res.sendStatus(200);
   }
