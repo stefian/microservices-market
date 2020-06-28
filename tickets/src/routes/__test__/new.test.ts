0cb329dc113c2b8bc6b1144b5eface8085d11902
stefian <stefan.ianta@w3ai.net>
2020-06-28 08:48:51 -0300

    + it("creates a ticket with valid inputs", async () => { # basic test w/o checking record in db

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -61,4 +61,14 @@ it("returns an error if an invalid price is provided", async () => {
     .expect(400);
 });
 
-it("creates a ticket with valid inputs", async () => {});
+it("creates a ticket with valid inputs", async () => {
+  // add in a check to make sure a ticket was saved
+
+  await request(app)
+    .post("/api/tickets")
+    .send({
+      title: "valid",
+      price: 20,
+    })
+    .expect(201);
+});
