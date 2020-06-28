f4700d6ddc9409aab88b2ff78017f93fd0fe32d7
stefian <stefan.ianta@w3ai.net>
2020-06-28 14:42:52 -0300

    + let tickets = await Ticket.find({}); # write test ticket to db and check new record

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,5 +1,6 @@
 import request from "supertest";
 import { app } from "../../app";
+import { Ticket } from "../../models/ticket";
 
 it("has a route handler listening to /api/tickets for post requests", async () => {
   const response = await request(app).post("/api/tickets").send({});
@@ -62,13 +63,22 @@ it("returns an error if an invalid price is provided", async () => {
 });
 
 it("creates a ticket with valid inputs", async () => {
-  // add in a check to make sure a ticket was saved
+  let tickets = await Ticket.find({}); // find all tickets in collection - there should be none since we delete before each test
+  expect(tickets.length).toEqual(0);
+
+  const title = "test-title";
 
   await request(app)
     .post("/api/tickets")
+    .set("Cookie", global.signin())
     .send({
-      title: "valid",
+      title,
       price: 20,
     })
     .expect(201);
+
+  tickets = await Ticket.find({});
+  expect(tickets.length).toEqual(1);
+  expect(tickets[0].price).toEqual(20);
+  expect(tickets[0].title).toEqual(title);
 });
