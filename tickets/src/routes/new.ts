e903ffab8487884dc17877690d3d944c74d80232
stefian <stefan.ianta@w3ai.net>
2020-06-28 08:40:30 -0300

    + body("price").isFloat({ gt: 0 }).withMessage("Price must be greater than 0"),

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -7,7 +7,12 @@ const router = express.Router();
 router.post(
   "/api/tickets",
   requireAuth,
-  [body("title").not().isEmpty().withMessage("Title is required")],
+  [
+    body("title").not().isEmpty().withMessage("Title is required"),
+    body("price")
+      .isFloat({ gt: 0 })
+      .withMessage("Price must be greater than 0"),
+  ],
   validateRequest,
   (req: Request, res: Response) => {
     res.sendStatus(200);
