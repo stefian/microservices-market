ec899c2d11c46ec6ca0557c06d6c0c8b8c317c7e
stefian <stefan.ianta@w3ai.net>
2020-06-29 11:31:41 -0300

    + app.use(updateTicketRouter);

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -10,6 +10,7 @@ import {
 import { createTicketRouter } from "./routes/new";
 import { showTicketRouter } from "./routes/show";
 import { indexTicketRouter } from "./routes/index";
+import { updateTicketRouter } from "./routes/update";
 
 const app = express();
 app.set("trust proxy", true); // to allow traffic through ingress-nginx
@@ -25,6 +26,7 @@ app.use(currentUser);
 app.use(createTicketRouter);
 app.use(showTicketRouter);
 app.use(indexTicketRouter);
+app.use(updateTicketRouter);
 
 app.all("*", async (req, res) => {
   throw new NotFoundError();
