bb10abeab806f022a3ecc2f9c4fcaae876c3f1fd
stefian <stefan.ianta@w3ai.net>
2020-06-29 10:16:57 -0300

    + it("returns a 401 if the user is not authenticated", async () => {

--- tickets/src/routes/__test__/update.test.ts
+++ tickets/src/routes/__test__/update.test.ts
@@ -1,9 +1,31 @@
 import request from "supertest";
 import { app } from "../../app";
+import mongoose from "mongoose";
 
-it("returns a 404 if the provided id does not exist", async () => {});
+it("returns a 404 if the provided id does not exist", async () => {
+  const id = new mongoose.Types.ObjectId().toHexString();
 
-it("returns a 401 if the user is not authenticated", async () => {});
+  await request(app)
+    .put(`/api/tickets${id}`)
+    .set("Cookie", global.signin())
+    .send({
+      title: "test title",
+      price: 20,
+    })
+    .expect(404); // since that generated ticket does not exist
+});
+
+it("returns a 401 if the user is not authenticated", async () => {
+  const id = new mongoose.Types.ObjectId().toHexString();
+
+  await request(app)
+    .put(`/api/tickets${id}`)
+    .send({
+      title: "test title",
+      price: 20,
+    })
+    .expect(401);
+});
 
 it("returns a 401 if the user does not own the ticket", async () => {});
 
