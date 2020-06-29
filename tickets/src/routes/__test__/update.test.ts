df2f821903ed2ffa42a963653ae747f92383b7a0
stefian <stefan.ianta@w3ai.net>
2020-06-29 13:31:11 -0300

    + it("returns a 400 if the user provides an invalid title or price", async () => {

--- tickets/src/routes/__test__/update.test.ts
+++ tickets/src/routes/__test__/update.test.ts
@@ -46,6 +46,34 @@ it("returns a 401 if the user does not own the ticket", async () => {
     .expect(401);
 });
 
-it("returns a 400 if the user provides an invalid title or price", async () => {});
+it("returns a 400 if the user provides an invalid title or price", async () => {
+  const cookie = global.signin();
+
+  const response = await request(app)
+    .post("/api/tickets")
+    .set("Cookie", cookie)
+    .send({
+      title: "asldkfj",
+      price: 20,
+    });
+
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set("Cookie", cookie)
+    .send({
+      title: "",
+      price: 20,
+    })
+    .expect(400);
+
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set("Cookie", cookie)
+    .send({
+      title: "valid title",
+      price: -10,
+    })
+    .expect(400);
+});
 
 it("updates the ticket provided valid inputs", async () => {});
