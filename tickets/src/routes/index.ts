05b8be00c52d54a62342e3a141e81602a7895263
stefian <stefan.ianta@w3ai.net>
2020-06-29 09:25:24 -0300

    + router.get("/api/tickets", async (req: Request, res: Response) => {

---
+++ tickets/src/routes/index.ts
@@ -0,0 +1,10 @@
+import express, { Request, Response } from "express";
+import { Ticket } from "../models/ticket";
+
+const router = express.Router();
+
+router.get("/api/tickets", async (req: Request, res: Response) => {
+  const tickets = await Ticket.find({}); // empty obj means all collection
+
+  res.send(tickets);
+});
