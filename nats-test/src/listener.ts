e08c1e27bd97336b9202feab475df7947292cba2
stefian <stefan.ianta@w3ai.net>
2020-07-02 19:25:21 -0300

    + abstract class Listener {

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,4 +1,4 @@
-import nats, { Message } from "node-nats-streaming";
+import nats, { Message, Stan } from "node-nats-streaming";
 import { randomBytes } from "crypto";
 
 console.clear();
@@ -30,7 +30,9 @@ stan.on("connect", () => {
   subscription.on("message", (msg: Message) => {
     const data = msg.getData();
 
+    // string or buffer type ?
     if (typeof data === "string") {
+      // event / msg processing code !
       console.log(
         `Received event #${msg.getSequence()}, with data: ${data}`
       );
@@ -42,3 +44,49 @@ stan.on("connect", () => {
 
 process.on("SIGINT", () => stan.close()); // Interrupt signal / CTRL-C => first close client connection
 process.on("SIGTERM", () => stan.close()); // Terminate signal => first close client connection
+
+abstract class Listener {
+  abstract subject: string;
+  abstract queueGroupName: string; // same as DurableName
+  abstract onMessage(data: any, msg: Message): void; // ToDo ?! 1000 - 9000 Market error codes !!!
+  private client: Stan;
+  protected ackWait = 5 * 1000; // 5000ms = 5 seconds
+
+  constructor(client: Stan) {
+    this.client = client;
+  }
+
+  subscriptionOptions() {
+    return this.client
+      .subscriptionOptions()
+      .setDeliverAllAvailable()
+      .setManualAckMode(true)
+      .setAckWait(this.ackWait)
+      .setDurableName(this.queueGroupName);
+  }
+
+  listen() {
+    const subscription = this.client.subscribe(
+      this.subject,
+      this.queueGroupName,
+      this.subscriptionOptions()
+    );
+
+    subscription.on("message", (msg: Message) => {
+      console.log(
+        `Message received: ${this.subject} / ${this.queueGroupName}`
+      );
+
+      const parsedData = this.parseMessage(msg);
+      this.onMessage(parsedData, msg);
+    });
+  }
+
+  parseMessage(msg: Message) {
+    const data = msg.getData();
+
+    return typeof data === "string"
+      ? JSON.parse(data)
+      : JSON.parse(data.toString("utf8")); // 'utf8' in case of type buffer
+  }
+}
