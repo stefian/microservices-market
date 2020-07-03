e7c91ca8ec272e356dd6092d4f931e10b381be5c
stefian <stefan.ianta@w3ai.net>
2020-07-03 16:27:05 -0300

    + stan.on("connect", async () => { # added async / await for waiting to message to be published

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -7,15 +7,19 @@ const stan = nats.connect("aibazar", "abc", {
   url: "http://localhost:4222",
 }); // in docs client is named: stan / nats backwards
 
-stan.on("connect", () => {
+stan.on("connect", async () => {
   console.log("Publisher connected to NATS");
 
   const publisher = new TicketCreatedPublisher(stan);
-  publisher.publish({
-    id: "123",
-    title: "service",
-    price: 20,
-  });
+  try {
+    await publisher.publish({
+      id: "123",
+      title: "service",
+      price: 20,
+    });
+  } catch (err) {
+    console.error(err);
+  }
 
   // const data = JSON.stringify({
   //   id: "123",
