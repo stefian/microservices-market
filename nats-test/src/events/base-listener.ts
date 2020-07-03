16d8599e66f6a94098e061f98628db6451aa7517
stefian <stefan.ianta@w3ai.net>
2020-07-03 08:51:23 -0300

    + import { TicketCreatedListener } from "./events/ticket-created-listener"; # refactored abstract Listener class and TicketCreatedListener subclass into the events folder

---
+++ nats-test/src/events/base-listener.ts
@@ -0,0 +1,47 @@
+import { Message, Stan } from "node-nats-streaming";
+
+export abstract class Listener {
+  abstract subject: string;
+  abstract queueGroupName: string; // same as DurableName
+  abstract onMessage(data: any, msg: Message): void; // ToDo ?! 1000 - 9000 Market error codes !!!
+  private client: Stan;
+  protected ackWait = 5 * 1000; // 5000ms = 5 seconds
+
+  constructor(client: Stan) {
+    this.client = client;
+  }
+
+  subscriptionOptions() {
+    return this.client
+      .subscriptionOptions()
+      .setDeliverAllAvailable()
+      .setManualAckMode(true)
+      .setAckWait(this.ackWait)
+      .setDurableName(this.queueGroupName);
+  }
+
+  listen() {
+    const subscription = this.client.subscribe(
+      this.subject,
+      this.queueGroupName,
+      this.subscriptionOptions()
+    );
+
+    subscription.on("message", (msg: Message) => {
+      console.log(
+        `Message received: ${this.subject} / ${this.queueGroupName}`
+      );
+
+      const parsedData = this.parseMessage(msg);
+      this.onMessage(parsedData, msg);
+    });
+  }
+
+  parseMessage(msg: Message) {
+    const data = msg.getData();
+
+    return typeof data === "string"
+      ? JSON.parse(data)
+      : JSON.parse(data.toString("utf8")); // 'utf8' in case of type buffer
+  }
+}
--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,5 +1,6 @@
-import nats, { Message, Stan } from "node-nats-streaming";
+import nats from "node-nats-streaming";
 import { randomBytes } from "crypto";
+import { TicketCreatedListener } from "./events/ticket-created-listener";
 
 console.clear();
 
@@ -20,60 +21,3 @@ stan.on("connect", () => {
 
 process.on("SIGINT", () => stan.close()); // Interrupt signal / CTRL-C => first close client connection
 process.on("SIGTERM", () => stan.close()); // Terminate signal => first close client connection
-
-abstract class Listener {
-  abstract subject: string;
-  abstract queueGroupName: string; // same as DurableName
-  abstract onMessage(data: any, msg: Message): void; // ToDo ?! 1000 - 9000 Market error codes !!!
-  private client: Stan;
-  protected ackWait = 5 * 1000; // 5000ms = 5 seconds
-
-  constructor(client: Stan) {
-    this.client = client;
-  }
-
-  subscriptionOptions() {
-    return this.client
-      .subscriptionOptions()
-      .setDeliverAllAvailable()
-      .setManualAckMode(true)
-      .setAckWait(this.ackWait)
-      .setDurableName(this.queueGroupName);
-  }
-
-  listen() {
-    const subscription = this.client.subscribe(
-      this.subject,
-      this.queueGroupName,
-      this.subscriptionOptions()
-    );
-
-    subscription.on("message", (msg: Message) => {
-      console.log(
-        `Message received: ${this.subject} / ${this.queueGroupName}`
-      );
-
-      const parsedData = this.parseMessage(msg);
-      this.onMessage(parsedData, msg);
-    });
-  }
-
-  parseMessage(msg: Message) {
-    const data = msg.getData();
-
-    return typeof data === "string"
-      ? JSON.parse(data)
-      : JSON.parse(data.toString("utf8")); // 'utf8' in case of type buffer
-  }
-}
-
-class TicketCreatedListener extends Listener {
-  subject = "ticket:created";
-  queueGroupName = "payments-service";
-
-  onMessage(data: any, msg: Message) {
-    console.log("Event data!", data);
-
-    msg.ack();
-  }
-}
