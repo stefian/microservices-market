a175a37a7cc1a38ee1964805c8330414d6aeadf9
stefian <stefan.ianta@w3ai.net>
2020-07-06 08:03:05 -0300

    + connect(clusterId: string, clientId: string, url: string) { # implemented the connect function

--- tickets/src/nats-wrapper.ts
+++ tickets/src/nats-wrapper.ts
@@ -1,5 +1,22 @@
 import nats, { Stan } from "node-nats-streaming";
 
-class NatsWrapper {}
+class NatsWrapper {
+  private _client?: Stan; // ? tells TS that _client might be undefined for some time
+
+  // connect needs a cluster Id, client Id and the Url to connect to
+  connect(clusterId: string, clientId: string, url: string) {
+    this._client = nats.connect(clusterId, clientId, { url });
+
+    return new Promise((resolve, reject) => {
+      this._client!.on("connect", () => {
+        console.log("Connected to NATS");
+        resolve();
+      });
+      this._client!.on("error", (err) => {
+        reject(err);
+      });
+    });
+  }
+}
 
 export const natsWrapper = new NatsWrapper();
