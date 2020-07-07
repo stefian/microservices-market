587132407e135cbe9718678d3dc264aced09e61e
stefian <stefan.ianta@w3ai.net>
2020-07-06 21:51:05 -0300

    + new TicketUpdatedPublisher(natsWrapper.client).publish({

--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -7,6 +7,8 @@ import {
   NotAuthorizedError,
 } from "@w3ai/common";
 import { Ticket } from "../models/ticket";
+import { TicketUpdatedPublisher } from "../events/publishers/ticket-updated-publisher";
+import { natsWrapper } from "../nats-wrapper";
 
 const router = express.Router();
 
@@ -38,6 +40,12 @@ router.put(
       price: req.body.price,
     }); // makes the updates in memory, not persisted to db
     await ticket.save(); // persist in db
+    new TicketUpdatedPublisher(natsWrapper.client).publish({
+      id: ticket.id,
+      title: ticket.title,
+      price: ticket.price,
+      userId: ticket.userId,
+    });
 
     res.send(ticket);
   }
