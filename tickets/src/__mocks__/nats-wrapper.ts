d5296667b69c28b88e1f63c31d387c1cdb6e693d
stefian <stefan.ianta@w3ai.net>
2020-07-07 15:21:04 -0300

    + publish: jest.fn().mockImplementation(

--- tickets/src/__mocks__/nats-wrapper.ts
+++ tickets/src/__mocks__/nats-wrapper.ts
@@ -1,11 +1,11 @@
 export const natsWrapper = {
   client: {
-    publish: (
-      subject: string,
-      data: string,
-      callback: () => void
-    ) => {
-      callback();
-    },
+    publish: jest
+      .fn()
+      .mockImplementation(
+        (subject: string, data: string, callback: () => void) => {
+          callback();
+        }
+      ),
   },
 };
