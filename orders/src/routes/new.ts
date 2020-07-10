5292f7e4be78382c4a775f4fc92dd2508505d68e
stefian <stefan.ianta@w3ai.net>
2020-07-10 08:44:36 -0300

    planning the tasks for the new order route handler

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -2,6 +2,8 @@ import mongoose from "mongoose";
 import express, { Request, Response } from "express";
 import { requireAuth, validateRequest } from "@w3ai/common";
 import { body } from "express-validator";
+import { Ticket } from "../models/ticket";
+import { Order } from "../models/order";
 
 const router = express.Router();
 
@@ -19,6 +21,16 @@ router.post(
   ],
   validateRequest,
   async (req: Request, res: Response) => {
+    // Find the ticket the user is trying to order in the database
+
+    // Make sure that this ticket is not already reserved - Many users might try to buy the same ticket at the same time
+
+    // calculate an expiration date for this order
+
+    // Build the order and save it to the database
+
+    // Publish an event saying that an order was created
+
     res.send({});
   }
 );
