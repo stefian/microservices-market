0fc2838930cef1bc651433ce45df4ddb2a6ecad8
stefian <stefan.ianta@w3ai.net>
2020-07-11 20:12:05 -0300

    + it("marks an order as cancelled", async () => { # + 4 comments / steps to implement the delete order test

---
+++ orders/src/routes/__test__/delete.test.ts
@@ -0,0 +1,10 @@
+import request from "supertest";
+import { app } from "../../app";
+import { Ticket } from "../../models/ticket";
+
+it("marks an order as cancelled", async () => {
+  // create a ticket with Ticket model
+  // make a request to create an order
+  // make a request to cancel the order
+  // expectation to make sure the thing is cancelled
+});
