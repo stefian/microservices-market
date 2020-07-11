ff040d286fed89b3b7f808ffe33530637b0d0931
stefian <stefan.ianta@w3ai.net>
2020-07-11 08:45:14 -0300

    +   // Create 2 orders as User 2 # Created 2 users and 3 orders for testing index.ts

--- orders/src/routes/__test__/index.test.ts
+++ orders/src/routes/__test__/index.test.ts
@@ -19,8 +19,27 @@ it("fetches orders for a particular user", async () => {
   const ticketTwo = await buildTicket();
   const ticketThree = await buildTicket();
 
+  const userOne = global.signin();
+  const userTwo = global.signin();
   // Create 1 order as User #1
+  await request(app)
+    .post("/api/orders")
+    .set("Cookie", userOne)
+    .send({ ticketId: ticketOne.id })
+    .expect(201);
+
   // Create 2 orders as User #2
+  await request(app)
+    .post("/api/orders")
+    .set("Cookie", userTwo)
+    .send({ ticketId: ticketTwo.id })
+    .expect(201);
+  await request(app)
+    .post("/api/orders")
+    .set("Cookie", userTwo)
+    .send({ ticketId: ticketThree.id })
+    .expect(201);
+
   // Make request to get orders for User #2
   // Make sure we only got the orders for User #2
 });
