9779e7485a0c341d53a4c781cbd208016895675e
stefian <stefan.ianta@w3ai.net>
2020-07-11 17:26:40 -0300

    + expect(fetchedOrder.id).toEqual(order.id); # implemented the test for fetching the order successfuly

--- orders/src/routes/__test__/show.test.ts
+++ orders/src/routes/__test__/show.test.ts
@@ -4,6 +4,26 @@ import { Ticket } from "../../models/ticket";
 
 it("fetches the order", async () => {
   // Create a ticket
+  const ticket = Ticket.build({
+    title: "service",
+    price: 20,
+  });
+  await ticket.save();
+
+  const user = global.signin();
   // Make a request to build an order with this ticket
+  const { body: order } = await request(app)
+    .post("/api/orders")
+    .set("Cookie", user)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
   // Make request to fetch the order
+  const { body: fetchedOrder } = await request(app)
+    .get(`/api/orders/${order.id}`)
+    .set("Cookie", user)
+    .send()
+    .expect(200);
+
+  expect(fetchedOrder.id).toEqual(order.id);
 });
