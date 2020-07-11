ad39a0e2e3123440f33bced7e9ef9709b9822396
stefian <stefan.ianta@w3ai.net>
2020-07-11 08:06:27 -0300

    + const buildTicket = async () => { # added helper function for building a ticket

--- orders/src/routes/__test__/index.test.ts
+++ orders/src/routes/__test__/index.test.ts
@@ -3,8 +3,22 @@ import { app } from "../../app";
 import { Order } from "../../models/order";
 import { Ticket } from "../../models/ticket";
 
+const buildTicket = async () => {
+  const ticket = Ticket.build({
+    title: "service",
+    price: 20,
+  });
+  await ticket.save();
+
+  return ticket;
+};
+
 it("fetches orders for a particular user", async () => {
   // Create 3 tickets
+  const ticketOne = await buildTicket();
+  const ticketTwo = await buildTicket();
+  const ticketThree = await buildTicket();
+
   // Create 1 order as User #1
   // Create 2 orders as User #2
   // Make request to get orders for User #2
