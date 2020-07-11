25c8e3f11ee043cdc00ede48a07829b406a43ef8
stefian <stefan.ianta@w3ai.net>
2020-07-11 15:39:56 -0300

    + it("fetches the order", async () => {

---
+++ orders/src/routes/__test__/show.test.ts
@@ -0,0 +1,9 @@
+import request from "supertest";
+import { app } from "../../app";
+import { Ticket } from "../../models/ticket";
+
+it("fetches the order", async () => {
+  // Create a ticket
+  // Make a request to build an order with this ticket
+  // Make request to fetch the order
+});
