6937e6d59fa9681a14b7c35964610b052251cb93
stefian <stefan.ianta@w3ai.net>
2020-07-12 15:49:56 -0300

    + export class OrderCreatedPublisher extends Publisher<

---
+++ orders/src/events/publishers/order-created-publisher.ts
@@ -0,0 +1,7 @@
+import { Publisher, OrderCreatedEvent, Subjects } from "@w3ai/common";
+
+export class OrderCreatedPublisher extends Publisher<
+  OrderCreatedEvent
+> {
+  readonly subject = Subjects.OrderCreated;
+}
