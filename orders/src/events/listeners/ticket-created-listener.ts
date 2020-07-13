1c14648a31e091bbcd3da4e02f4cc0069d9ad658
stefian <stefan.ianta@w3ai.net>
2020-07-13 06:46:31 -0300

    + import { queueGroupName } from "./queue-group-name";

--- orders/src/events/listeners/ticket-created-listener.ts
+++ orders/src/events/listeners/ticket-created-listener.ts
@@ -1,12 +1,13 @@
 import { Message } from "node-nats-streaming";
 import { Subjects, Listener, TicketCreatedEvent } from "@w3ai/common";
 import { Ticket } from "../../models/ticket";
+import { queueGroupName } from "./queue-group-name";
 
 export class TicketCreatedListener extends Listener<
   TicketCreatedEvent
 > {
   readonly subject = Subjects.TicketCreated;
-  queueGroupName = "orders-service";
+  queueGroupName = queueGroupName;
 
   onMessage(data: TicketCreatedEvent["data"], msg: Message) {}
 }
