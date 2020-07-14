471537ed54940767f7be7fd620d283714d0693b5
stefian <stefan.ianta@w3ai.net>
2020-07-14 18:41:35 -0300

    + const setup = async () => { # prep setup() for ticket-updated-listener.test.ts

---
+++ orders/src/events/listeners/__test__/ticket-updated-listener.test.ts
@@ -0,0 +1,11 @@
+const setup = async () => {
+  // Create a listener
+  // Create and save a ticket
+  // Create a fake data object
+  // Create a fake msg object
+  // return all of this stuff
+};
+
+it("finds, updates, and saves a ticket", async () => {});
+
+it("acks the message", async () => {});
