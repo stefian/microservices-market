858adb17ab537e9884a777160b2229401c681800
stefian <stefan.ianta@w3ai.net>
2020-07-14 06:25:21 -0300

    + await secondInstance!.save(); # implemented the OCC test

--- tickets/src/models/__test__/ticket.test.ts
+++ tickets/src/models/__test__/ticket.test.ts
@@ -1,11 +1,33 @@
 import { Ticket } from "../ticket";
 
-it("implements optimistic concurrency control", async () => {
+it("implements optimistic concurrency control", async (done) => {
   // Create an instance of a ticket
+  const ticket = Ticket.build({
+    title: "service",
+    price: 5,
+    userId: "123",
+  });
+
   // Save the ticket to the database
+  await ticket.save();
+
   // Fetch the ticket twice
+  const firstInstance = await Ticket.findById(ticket.id);
+  const secondInstance = await Ticket.findById(ticket.id);
+
   // make two separate changes to the tickets
+  firstInstance!.set({ price: 10 });
+  secondInstance!.set({ price: 15 });
+
   // save the first fetched ticket
-  // save the second fetched ticket and expect an error
-  // - will have an outdated version nr
+  await firstInstance!.save();
+
+  // save the second fetched ticket and expect an error - due to an outdated version property
+  try {
+    await secondInstance!.save();
+  } catch (err) {
+    return done();
+  }
+
+  throw new Error("Should not reach this point");
 });
