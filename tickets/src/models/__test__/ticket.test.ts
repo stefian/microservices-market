47867cf8656ab62aa786380a0aaae2a9a0618492
stefian <stefan.ianta@w3ai.net>
2020-07-13 21:02:49 -0300

    +   // save the second fetched ticket and expect an error # planning the OCC test steps

--- tickets/src/models/__test__/ticket.test.ts
+++ tickets/src/models/__test__/ticket.test.ts
@@ -1,3 +1,11 @@
 import { Ticket } from "../ticket";
 
-it("implements optimistic concurrency control", async () => {});
+it("implements optimistic concurrency control", async () => {
+  // Create an instance of a ticket
+  // Save the ticket to the database
+  // Fetch the ticket twice
+  // make two separate changes to the tickets
+  // save the first fetched ticket
+  // save the second fetched ticket and expect an error
+  // - will have an outdated version nr
+});
