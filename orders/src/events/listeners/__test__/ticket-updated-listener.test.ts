6bc2afcda6b2f41558cbcba9b9083fcaba40d228
stefian <stefan.ianta@w3ai.net>
2020-07-14 19:02:47 -0300

    + return { msg, data, ticket, listener }; # implemented the ticket-updated-listener.test setup

--- orders/src/events/listeners/__test__/ticket-updated-listener.test.ts
+++ orders/src/events/listeners/__test__/ticket-updated-listener.test.ts
@@ -1,9 +1,39 @@
+import mongoose from "mongoose";
+import { Message } from "node-nats-streaming";
+import { TicketUpdatedEvent } from "@w3ai/common";
+import { TicketUpdatedListener } from "../ticket-updated-listener";
+import { natsWrapper } from "../../../nats-wrapper";
+import { Ticket } from "../../../models/ticket";
+
 const setup = async () => {
   // Create a listener
+  const listener = new TicketUpdatedListener(natsWrapper.client);
+
   // Create and save a ticket
+  const ticket = Ticket.build({
+    id: mongoose.Types.ObjectId().toHexString(),
+    title: "service",
+    price: 20,
+  });
+  await ticket.save();
+
   // Create a fake data object
+  const data: TicketUpdatedEvent["data"] = {
+    id: ticket.id,
+    version: ticket.version + 1,
+    title: "new service",
+    price: 999,
+    userId: "whateverId",
+  };
+
   // Create a fake msg object
+  // @ts-ignore
+  const msg: Message = {
+    ack: jest.fn(),
+  };
+
   // return all of this stuff
+  return { msg, data, ticket, listener };
 };
 
 it("finds, updates, and saves a ticket", async () => {});
