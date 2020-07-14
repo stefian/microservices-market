4197e97b864af7f8d26ed588e70df834af6b2f0a
stefian <stefan.ianta@w3ai.net>
2020-07-14 17:52:29 -0300

    + const listener = new TicketCreatedListener(natsWrapper.client);

--- orders/src/events/listeners/__test__/ticket-created-listener.test.ts
+++ orders/src/events/listeners/__test__/ticket-created-listener.test.ts
@@ -1,7 +1,29 @@
+import { Message } from "node-nats-streaming";
+import mongoose from "mongoose";
+import { TicketCreatedEvent } from "@w3ai/common";
+import { TicketCreatedListener } from "../ticket-created-listener";
+import { natsWrapper } from "../../../nats-wrapper";
+
 const setup = async () => {
   // create an instance of the listener
+  const listener = new TicketCreatedListener(natsWrapper.client);
+
   // create a fake data event
+  const data: TicketCreatedEvent["data"] = {
+    version: 0,
+    id: new mongoose.Types.ObjectId().toHexString(),
+    title: "service",
+    price: 10,
+    userId: new mongoose.Types.ObjectId().toHexString(),
+  };
+
   // create a fake message object
+  // @ts-ignore
+  const msg: Message = {
+    ack: jest.fn(), // a jest mock function to keep track of nr calls and params provided
+  };
+
+  return { listener, data, msg };
 };
 
 it("creates and saves a ticket", async () => {
