fc70d9473b3fc420f04768fc2cd71266d516f019
stefian <stefan.ianta@w3ai.net>
2020-07-14 22:29:48 -0300

    + id: mongoose.Types.ObjectId().toHexString(),

--- orders/src/routes/__test__/delete.test.ts
+++ orders/src/routes/__test__/delete.test.ts
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import request from "supertest";
 import { app } from "../../app";
 import { Ticket } from "../../models/ticket";
@@ -7,6 +8,7 @@ import { natsWrapper } from "../../nats-wrapper";
 it("marks an order as cancelled", async () => {
   // create a ticket with Ticket model
   const ticket = Ticket.build({
+    id: mongoose.Types.ObjectId().toHexString(),
     title: "service",
     price: 20,
   });
@@ -35,6 +37,7 @@ it("marks an order as cancelled", async () => {
 
 it("emits an order cancelled event", async () => {
   const ticket = Ticket.build({
+    id: mongoose.Types.ObjectId().toHexString(),
     title: "service",
     price: 20,
   });
