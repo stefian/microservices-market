a1578c7273249f2e59bf8c709743a5402b6cf536
stefian <stefan.ianta@w3ai.net>
2020-07-15 08:17:30 -0300

    + async onMessage(data: OrderCreatedEvent["data"], msg: Message) {

--- tickets/src/events/listeners/order-created-listener.ts
+++ tickets/src/events/listeners/order-created-listener.ts
@@ -1,6 +1,7 @@
 import { Message } from "node-nats-streaming";
 import { Listener, OrderCreatedEvent, Subjects } from "@w3ai/common";
 import { queueGroupName } from "./queue-group-name";
+import { Ticket } from "../../models/ticket";
 
 export class OrderCreatedListener extends Listener<
   OrderCreatedEvent
@@ -8,5 +9,11 @@ export class OrderCreatedListener extends Listener<
   readonly subject = Subjects.OrderCreated;
   queueGroupName = queueGroupName;
 
-  async onMessage(data: OrderCreatedEvent["data"], msg: Message) {}
+  async onMessage(data: OrderCreatedEvent["data"], msg: Message) {
+    // Find the ticket that the order is reserving
+    // If no ticket, throw error
+    // Mark the ticket as being reserved by setting its orderId property
+    // Save the ticket
+    // ack the message
+  }
 }
