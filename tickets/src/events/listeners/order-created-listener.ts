34870e13bf3272b5baef9a8e44c633e18de2fc38
stefian <stefan.ianta@w3ai.net>
2020-07-15 08:30:23 -0300

    + const ticket = await Ticket.findById(data.ticket.id);

--- tickets/src/events/listeners/order-created-listener.ts
+++ tickets/src/events/listeners/order-created-listener.ts
@@ -11,9 +11,20 @@ export class OrderCreatedListener extends Listener<
 
   async onMessage(data: OrderCreatedEvent["data"], msg: Message) {
     // Find the ticket that the order is reserving
+    const ticket = await Ticket.findById(data.ticket.id);
+
     // If no ticket, throw error
+    if (!ticket) {
+      throw new Error("Ticket not found");
+    }
+
     // Mark the ticket as being reserved by setting its orderId property
+    ticket.set({ orderId: data.id });
+
     // Save the ticket
+    await ticket.save();
+
     // ack the message
+    msg.ack();
   }
 }
