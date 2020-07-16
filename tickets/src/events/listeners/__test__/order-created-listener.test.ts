c819c4948cbe8ba0474f50450d0c2100a4f02123
stefian <stefan.ianta@w3ai.net>
2020-07-15 22:10:36 -0300

    + it("publishes a ticket updated event", async () => {

--- tickets/src/events/listeners/__test__/order-created-listener.test.ts
+++ tickets/src/events/listeners/__test__/order-created-listener.test.ts
@@ -55,3 +55,15 @@ it("acks the message", async () => {
 
   expect(msg.ack).toHaveBeenCalled();
 });
+
+it("publishes a ticket updated event", async () => {
+  const { listener, ticket, data, msg } = await setup();
+
+  await listener.onMessage(data, msg);
+
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
+
+  // @ts-ignore
+  console.log(natsWrapper.client.publish.mock.calls[0][1]);
+  // console.log(natsWrapper.client.publish.mock.calls);
+});
