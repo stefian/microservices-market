6bd02c214cc5463c4327a9e7b00bef0ebf3402d9
stefian <stefan.ianta@w3ai.net>
2020-07-16 20:55:44 -0300

    cleanup index.ts

--- expiration/src/index.ts
+++ expiration/src/index.ts
@@ -1,16 +1,6 @@
-import mongoose from "mongoose";
-import { app } from "./app";
 import { natsWrapper } from "./nats-wrapper";
-import { OrderCreatedListener } from "./events/listeners/order-created-listener";
-import { OrderCancelledListener } from "./events/listeners/order-cancelled-listener";
 
 const start = async () => {
-  if (!process.env.JWT_KEY) {
-    throw new Error("JWT_KEY must be defined");
-  }
-  if (!process.env.MONGO_URI) {
-    throw new Error("MONGO_URI must be defined");
-  }
   if (!process.env.NATS_CLIENT_ID) {
     throw new Error("NATS_CLIENT_ID must be defined");
   }
@@ -34,23 +24,9 @@ const start = async () => {
     });
     process.on("SIGINT", () => natsWrapper.client.close());
     process.on("SIGTERM", () => natsWrapper.client.close());
-
-    new OrderCreatedListener(natsWrapper.client).listen();
-    new OrderCancelledListener(natsWrapper.client).listen();
-
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-    });
-    console.log("Connected to MongoDB");
   } catch (err) {
     console.error(err);
   }
-
-  app.listen(3000, () => {
-    console.log("Tickets: Listening on port 3000!");
-  });
 };
 
 start();
