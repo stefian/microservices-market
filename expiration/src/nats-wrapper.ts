811f254a1d4ddc54c9f539f419173e4d6bd29c0e
stefian <stefan.ianta@w3ai.net>
2020-07-16 20:29:42 -0300

    added index.ts nats-wrapper and mocks from the tickets src/

---
+++ expiration/src/nats-wrapper.ts
@@ -0,0 +1,29 @@
+import nats, { Stan } from "node-nats-streaming";
+
+class NatsWrapper {
+  private _client?: Stan; // ? tells TS that _client might be undefined for some time
+
+  get client() {
+    if (!this._client) {
+      throw new Error("Cannot access NATS client before connecting");
+    }
+
+    return this._client;
+  }
+  // connect needs a cluster Id, client Id and the Url to connect to
+  connect(clusterId: string, clientId: string, url: string) {
+    this._client = nats.connect(clusterId, clientId, { url });
+
+    return new Promise((resolve, reject) => {
+      this.client.on("connect", () => {
+        console.log("Connected to NATS");
+        resolve();
+      });
+      this.client.on("error", (err) => {
+        reject(err);
+      });
+    });
+  }
+}
+
+export const natsWrapper = new NatsWrapper();
