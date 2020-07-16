1d4dba36708bb0fe68354ea4bc4d645a278d59a3
stefian <stefan.ianta@w3ai.net>
2020-07-16 08:29:09 -0300

    + const setup = async () => {

---
+++ tickets/src/events/listeners/__test__/order-cancelled-listener.test.ts
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+import { Message } from "node-nats-streaming";
+import { OrderCancelledEvent } from "@w3ai/common";
+import { OrderCancelledListener } from "../order-cancelled-listener";
+import { natsWrapper } from "../../../nats-wrapper";
+import { Ticket } from "../../../models/ticket";
+
+const setup = async () => {
+  const listener = new OrderCancelledListener(natsWrapper.client);
+
+  const orderId = mongoose.Types.ObjectId().toHexString();
+  const ticket = Ticket.build({
+    title: "service",
+    price: 20,
+    userId: "asdf",
+  });
+  ticket.set({ orderId });
+  await ticket.save();
+
+  const data: OrderCancelledEvent["data"] = {
+    id: orderId,
+    version: 0,
+    ticket: {
+      id: ticket.id,
+    },
+  };
+
+  // @ts-ignore
+  const msg: Message = {
+    ack: jest.fn(),
+  };
+
+  return { msg, data, ticket, orderId, listener };
+};
