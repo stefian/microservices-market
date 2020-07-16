5cc01299f76ca97b8dd38fdab99d23353cef8801
stefian <stefan.ianta@w3ai.net>
2020-07-16 08:47:45 -0300

    + it("updates the ticket, publishes an event, and acks the message", async () => {

--- tickets/src/events/listeners/__test__/order-cancelled-listener.test.ts
+++ tickets/src/events/listeners/__test__/order-cancelled-listener.test.ts
@@ -32,3 +32,15 @@ const setup = async () => {
 
   return { msg, data, ticket, orderId, listener };
 };
+
+it("updates the ticket, publishes an event, and acks the message", async () => {
+  const { msg, data, ticket, orderId, listener } = await setup();
+
+  await listener.onMessage(data, msg);
+
+  const updatedTicket = await Ticket.findById(ticket.id);
+
+  expect(updatedTicket!.orderId).not.toBeDefined();
+  expect(msg.ack).toHaveBeenCalled();
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
+});
