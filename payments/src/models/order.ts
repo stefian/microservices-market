eca25546bc2cf5319dab211d2861eaf1ccd6b4db
stefian <stefan.ianta@w3ai.net>
2020-07-18 13:50:48 -0300

    + orderSchema.set("versionKey", "version");

--- payments/src/models/order.ts
+++ payments/src/models/order.ts
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { updateIfCurrentPlugin } from "mongoose-update-if-current";
 import { OrderStatus } from "@w3ai/common";
 
 interface OrderAttrs {
@@ -45,6 +46,9 @@ const orderSchema = new mongoose.Schema(
     },
   }
 );
+
+orderSchema.set("versionKey", "version");
+orderSchema.plugin(updateIfCurrentPlugin);
 
 orderSchema.statics.build = (attrs: OrderAttrs) => {
   return new Order({
