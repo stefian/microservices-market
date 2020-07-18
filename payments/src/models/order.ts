72f64bcb22ca85638893a575ba773162b73433d6
stefian <stefan.ianta@w3ai.net>
2020-07-18 13:42:07 -0300

    + orderSchema.statics.build = (attrs: OrderAttrs) => {

--- payments/src/models/order.ts
+++ payments/src/models/order.ts
@@ -45,3 +45,20 @@ const orderSchema = new mongoose.Schema(
     },
   }
 );
+
+orderSchema.statics.build = (attrs: OrderAttrs) => {
+  return new Order({
+    _id: attrs.id,
+    version: attrs.version,
+    price: attrs.price,
+    userId: attrs.userId,
+    status: attrs.status,
+  });
+};
+
+const Order = mongoose.model<OrderDoc, OrderModel>(
+  "Order",
+  orderSchema
+);
+
+export { Order };
