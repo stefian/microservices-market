1dca187ea49087621849c46087e785d457596258
stefian <stefan.ianta@w3ai.net>
2020-07-18 20:41:20 -0300

    + it("replicates the order info", async () => {

--- payments/src/events/listeners/__test__/order-created-listener.test.ts
+++ payments/src/events/listeners/__test__/order-created-listener.test.ts
@@ -3,6 +3,7 @@ import { Message } from "node-nats-streaming";
 import { OrderCreatedEvent, OrderStatus } from "@w3ai/common";
 import { natsWrapper } from "../../../nats-wrapper";
 import { OrderCreatedListener } from "../order-created-listener";
+import { Order } from "../../../models/order";
 
 const setup = async () => {
   const listener = new OrderCreatedListener(natsWrapper.client);
@@ -26,3 +27,19 @@ const setup = async () => {
 
   return { listener, data, msg };
 };
+
+it("replicates the order info", async () => {
+  const { listener, data, msg } = await setup();
+
+  await listener.onMessage(data, msg);
+
+  const order = await Order.findById(data.id);
+
+  expect(order!.price).toEqual(data.ticket.price);
+});
+
+it("acks the message", async () => {
+  const { listener, data, msg } = await setup();
+
+  await listener.onMessage(data, msg);
+});
