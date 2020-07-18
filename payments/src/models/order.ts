2d6f0dd1f67d2208c7e5b509bce84b7735c435c2
stefian <stefan.ianta@w3ai.net>
2020-07-18 12:04:57 -0300

    + interface OrderAttrs {

--- payments/src/models/order.ts
+++ payments/src/models/order.ts
@@ -1,6 +1,13 @@
 import mongoose from "mongoose";
+import { OrderStatus } from "@w3ai/common";
 
-interface OrderAttrs {}
+interface OrderAttrs {
+  id: string;
+  version: number;
+  userId: string;
+  price: number;
+  status: OrderStatus;
+}
 
 interface OrderDoc extends mongoose.Document {}
 
