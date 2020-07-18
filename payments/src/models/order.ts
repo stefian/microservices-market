3921b5f5216df85f7f7a9f703274ede020b2a66e
stefian <stefan.ianta@w3ai.net>
2020-07-18 13:09:21 -0300

    + const orderSchema = new mongoose.Schema(

--- payments/src/models/order.ts
+++ payments/src/models/order.ts
@@ -19,3 +19,29 @@ interface OrderDoc extends mongoose.Document {
 interface OrderModel extends mongoose.Model<OrderDoc> {
   build(attrs: OrderAttrs): OrderDoc;
 }
+
+const orderSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: Object.values(OrderStatus),
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+      },
+    },
+  }
+);
