264edebef9e6fddb834e00430ac26d9e94745b7c
stefian <stefan.ianta@w3ai.net>
2020-07-19 07:54:11 -0300

    + it('updates the status of the order', async () => {

--- payments/src/events/listeners/__test__/order-cancelled-listener.test.ts
+++ payments/src/events/listeners/__test__/order-cancelled-listener.test.ts
@@ -33,3 +33,19 @@ const setup = async () => {
   return { listener, data, msg, order };
 }
 
+it('updates the status of the order', async () => {
+  const { listener, data, msg, order } = await setup();
+
+  await listener.onMessage(data, msg);
+
+  const updatedOrder = await Order.findById(order.id);
+
+  expect(updatedOrder!.status).toEqual(OrderStatus.Cancelled);
+});
+
+it('acks the message', async () => {
+  const { listener, data, msg, order } = await setup();
+
+  await listener.onMessage(data, msg);
+});
+
