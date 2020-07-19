f9ea50bca8e5fe19f4310306afe02027bca2cb74
stefian <stefan.ianta@w3ai.net>
2020-07-19 07:56:55 -0300

    + id: mongoose.Types.ObjectId().toHexString(),

--- payments/src/events/listeners/__test__/order-cancelled-listener.test.ts
+++ payments/src/events/listeners/__test__/order-cancelled-listener.test.ts
@@ -1,39 +1,39 @@
-import mongoose from 'mongoose';
-import { Message } from 'node-nats-streaming';
-import { OrderStatus, OrderCancelledEvent } from '@w3ai/common';
-import { OrderCancelledListener } from '../order-cancelled-listener';
-import { natsWrapper } from '../../../nats-wrapper';
-import {Order} from '../../../models/order'
+import mongoose from "mongoose";
+import { Message } from "node-nats-streaming";
+import { OrderStatus, OrderCancelledEvent } from "@w3ai/common";
+import { OrderCancelledListener } from "../order-cancelled-listener";
+import { natsWrapper } from "../../../nats-wrapper";
+import { Order } from "../../../models/order";
 
 const setup = async () => {
   const listener = new OrderCancelledListener(natsWrapper.client);
 
   const order = Order.build({
-    id: mongoose.Types.ObjectId().toHexString();
+    id: mongoose.Types.ObjectId().toHexString(),
     status: OrderStatus.Created,
     price: 10,
-    userId: 'dsafa',
-    version: 0
+    userId: "dsafa",
+    version: 0,
   });
   await order.save();
 
-  const data: OrderCancelledEvent['data'] = {
+  const data: OrderCancelledEvent["data"] = {
     id: order.id,
     version: 1,
     ticket: {
-      id: 'sadagd'
-    }
+      id: "sadagd",
+    },
   };
 
   // @ts-ignore
   const msg: Message = {
-    ack: jest.fn()
+    ack: jest.fn(),
   };
 
   return { listener, data, msg, order };
-}
+};
 
-it('updates the status of the order', async () => {
+it("updates the status of the order", async () => {
   const { listener, data, msg, order } = await setup();
 
   await listener.onMessage(data, msg);
@@ -43,11 +43,10 @@ it('updates the status of the order', async () => {
   expect(updatedOrder!.status).toEqual(OrderStatus.Cancelled);
 });
 
-it('acks the message', async () => {
+it("acks the message", async () => {
   const { listener, data, msg, order } = await setup();
 
   await listener.onMessage(data, msg);
 
   expect(msg.ack).toHaveBeenCalled();
 });
-
