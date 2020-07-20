a0b618e9a52ce8feebeae05a310a5c72cd1c97cc
stefian <stefan.ianta@w3ai.net>
2020-07-20 11:15:47 -0300

    + id: id || new mongoose.Types.ObjectId().toHexString(),

--- payments/src/test/setup.ts
+++ payments/src/test/setup.ts
@@ -41,10 +41,10 @@ afterAll(async () => {
   await mongoose.connection.close();
 });
 
-global.signin = () => {
+global.signin = (id?: string) => {
   // Build a JWT payload. { id, email }
   const payload = {
-    id: new mongoose.Types.ObjectId().toHexString(),
+    id: id || new mongoose.Types.ObjectId().toHexString(),
     email: "test@test.com",
   };
 
