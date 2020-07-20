1b16b07e3df4a1ec50b25758362c6075593a2745
stefian <stefan.ianta@w3ai.net>
2020-07-20 11:03:08 -0300

    + it("returns a 401 when purchasing an order that doesnt belong to the user", async () => {

--- payments/src/routes/__test__/new.test.ts
+++ payments/src/routes/__test__/new.test.ts
@@ -1,10 +1,12 @@
 import mongoose from "mongoose";
 import request from "supertest";
 import { app } from "../../app";
+import { Order } from "../../models/order";
+import { OrderStatus } from "@w3ai/common";
 
 it("returns a 404 when puschasing an order that does not exist", async () => {
   await request(app)
-    .post("/app/payments")
+    .post("/api/payments")
     .set("Cookie", global.signin())
     .send({
       token: "afsfsf",
@@ -13,6 +15,24 @@ it("returns a 404 when puschasing an order that does not exist", async () => {
     .expect(404);
 });
 
-it("returns a 401 when purchasing an order that doesnt belong to the user", async () => {});
+it("returns a 401 when purchasing an order that doesnt belong to the user", async () => {
+  const order = Order.build({
+    id: mongoose.Types.ObjectId().toHexString(),
+    userId: mongoose.Types.ObjectId().toHexString(),
+    version: 0,
+    price: 20,
+    status: OrderStatus.Created,
+  });
+  await order.save();
+
+  await request(app)
+    .post("/api/payments")
+    .set("Cookie", global.signin())
+    .send({
+      token: "afsfsf",
+      orderId: order.id,
+    })
+    .expect(401);
+});
 
 it("returns a 400 when purchasing a cancelled order", async () => {});
