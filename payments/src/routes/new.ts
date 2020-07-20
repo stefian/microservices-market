68c98404d21f8f9812aaabeff03330dcafc0698f
stefian <stefan.ianta@w3ai.net>
2020-07-20 09:50:58 -0300

    + const order = await Order.findById(orderId);

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -5,6 +5,8 @@ import {
   validateRequest,
   BadRequestError,
   NotFoundError,
+  NotAuthorizedError,
+  OrderStatus,
 } from "@w3ai/common";
 import { Order } from "../models/order";
 
@@ -16,6 +18,20 @@ router.post(
   [body("token").not().isEmpty(), body("orderId").not().isEmpty()],
   validateRequest,
   async (req: Request, res: Response) => {
+    const { token, orderId } = req.body;
+
+    const order = await Order.findById(orderId);
+
+    if (!order) {
+      throw new NotFoundError();
+    }
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+    if (order.status === OrderStatus.Cancelled) {
+      throw new BadRequestError("Cannot pay for a cancelled order");
+    }
+
     res.send({ success: true });
   }
 );
