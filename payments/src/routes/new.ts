384b0bd550945c27474a5c67aaa56ad1f54785ec
stefian <stefan.ianta@w3ai.net>
2020-07-20 08:54:51 -0300

    + validateRequest,

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -14,6 +14,7 @@ router.post(
   "/api/payments",
   requireAuth,
   [body("token").not().isEmpty(), body("orderId").not().isEmpty()],
+  validateRequest,
   async (req: Request, res: Response) => {
     res.send({ success: true });
   }
