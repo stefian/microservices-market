2d09443df60e3a0d9de9acdcb68cd1a1b632a23f
stefian <stefan.ianta@w3ai.net>
2020-07-20 10:46:27 -0300

    + it("returns a 404 when puschasing an order that does not exist", async () => {

--- payments/src/routes/__test__/new.test.ts
+++ payments/src/routes/__test__/new.test.ts
@@ -1,7 +1,17 @@
+import mongoose from "mongoose";
 import request from "supertest";
 import { app } from "../../app";
 
-it("returns a 404 when puschasing an order that does not exist", async () => {});
+it("returns a 404 when puschasing an order that does not exist", async () => {
+  await request(app)
+    .post("/app/payments")
+    .set("Cookie", global.signin())
+    .send({
+      token: "afsfsf",
+      orderId: mongoose.Types.ObjectId().toHexString(),
+    })
+    .expect(404);
+});
 
 it("returns a 401 when purchasing an order that doesnt belong to the user", async () => {});
 
