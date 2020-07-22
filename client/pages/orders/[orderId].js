3d96f8c58d25968190a2233af9eea26d9aa02f57
stefian <stefan.ianta@w3ai.net>
2020-07-22 14:26:23 -0300

    + const { data } = await client.get(`/api/orders/${orderId}`);

--- client/pages/orders/[orderId].js
+++ client/pages/orders/[orderId].js
@@ -1,5 +1,12 @@
-const OrderShow = () => {
+const OrderShow = ({ order }) => {
   return <div>OrderShow</div>;
+};
+
+OrderShow.getInitialProps = async (context, client) => {
+  const { orderId } = context.query;  // like the title of the file [orderId]
+  const { data } = await client.get(`/api/orders/${orderId}`);
+
+  return { order: data };
 };
 
 export default OrderShow;
