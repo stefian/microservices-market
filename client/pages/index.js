83ed551e545fedda92afbd24b9a8a1fed43e373c
stefian <stefan.ianta@w3ai.net>
2020-07-22 11:03:49 -0300

    + const { data } = await client.get('/api/tickets');

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,10 +1,14 @@
-const LandingPage = ({ currentUser }) => { // request executed from the browser
+const LandingPage = ({ currentUser, tickets }) => { // request executed from the browser
+  console.log(tickets);
+
   return currentUser ? <h1>You are signed in</h1> : <h1>You are NOT signed in</h1>
 }
 
 // get the context, client axios and current user from the parent _app component
 LandingPage.getInitialProps = async (context, client, currentUser) => {  // Request executed on the server / client k8 pod
-  return {}
+  const { data } = await client.get('/api/tickets');
+
+  return { tickets: data };
 };
 
 export default LandingPage;
