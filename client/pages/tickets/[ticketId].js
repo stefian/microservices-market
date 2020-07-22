64c8e06cc56d5781180c0519a49c23447994142b
stefian <stefan.ianta@w3ai.net>
2020-07-22 13:19:07 -0300

    + onSuccess: (order) => console.log(order)

--- client/pages/tickets/[ticketId].js
+++ client/pages/tickets/[ticketId].js
@@ -1,7 +1,20 @@
+import useRequest from '../../hooks/use-request';
+
 const TicketShow = ({ ticket }) => {
+  const { doRequest, errors } = useRequest({
+    url: '/api/orders',
+    method: 'post',
+    body: {
+      ticketId: ticket.id
+    },
+    onSuccess: (order) => console.log(order)
+  });
+
   return <div>
     <h1>{ticket.title}</h1>
     <h4>Price: {ticket.price}</h4>
+    {errors}
+    <button onClick={doRequest} className="btn btn-primary">Purchase</button>
   </div>;
 };
 
