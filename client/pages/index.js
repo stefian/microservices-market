6295bdc3f8feecc65db639dbb20effc27bed44b6
stefian <stefan.ianta@w3ai.net>
2020-07-22 11:17:20 -0300

    + const ticketList = tickets.map(ticket => {

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,7 +1,29 @@
 const LandingPage = ({ currentUser, tickets }) => { // request executed from the browser
-  console.log(tickets);
+  const ticketList = tickets.map(ticket => {
+    return (
+      <tr key={ticket.id}>
+        <td>{ticket.title}</td>
+        <td>{ticket.price}</td>
+      </tr>
+    );
+  });
 
-  return currentUser ? <h1>You are signed in</h1> : <h1>You are NOT signed in</h1>
+  return (
+    <div>
+      <h1>Tickets</h1>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          {ticketList}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 // get the context, client axios and current user from the parent _app component
