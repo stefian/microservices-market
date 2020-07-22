88e91d2a50bb5b12830deff1b26512b49917a833
stefian <stefan.ianta@w3ai.net>
2020-07-22 19:54:45 -0300

    + onSuccess: () => Router.push('/orders'),

--- client/pages/orders/[orderId].js
+++ client/pages/orders/[orderId].js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import Router from 'next/router';
 import useRequest from '../../hooks/use-request';
 
 const OrderShow = ({ order, currentUser }) => {
@@ -10,7 +11,7 @@ const OrderShow = ({ order, currentUser }) => {
     body: {
       orderId: order.id
     },
-    onSuccess: (payment) => console.log(payment)
+    onSuccess: () => Router.push('/orders'),
   });
 
   useEffect(() => {
