89fcec99c794879a9027488f2880460008bc715c
stefian <stefan.ianta@w3ai.net>
2020-07-22 12:49:30 -0300

    + const { data } = await client.get(`/api/tickets/${ticketId}`);

--- client/pages/tickets/[ticketId].js
+++ client/pages/tickets/[ticketId].js
@@ -1,5 +1,15 @@
-const TicketShow = () => {
-  return <div>TicketShow</div>;
+const TicketShow = ({ ticket }) => {
+  return <div>
+    <h1>{ticket.title}</h1>
+    <h4>Price: {ticket.price}</h4>
+  </div>;
+};
+
+TicketShow.getInitialProps = async (context, client) => {
+  const { ticketId } = context.query; // ticketId ~ like in the filename [ticketId].js
+  const { data } = await client.get(`/api/tickets/${ticketId}`);
+
+  return { ticket: data }
 };
 
 export default TicketShow;
